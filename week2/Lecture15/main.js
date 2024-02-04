bcb7ea7a866b709bb58c205e78f3cf32ef900e5f
Van Raj Thakur <vanrajthakur200604@gmail.com>
2024-02-04 17:45:13 +0530

    added lecture 14 of course in Lecture15

---
+++ week2/Lecture15/main.js
@@ -0,0 +1,32 @@
+// console.log("Hello World!")
+
+/**
+ * DataTypes
+ * 1. Number
+ * 2. Strings
+ * 3. Null
+ * 4. Booleans
+ * 5. Undefined
+ * 6. Objects
+ * 7. Symbols [end]
+ */
+
+// Syntax and Declaration
+var score = 10;       // ES-5
+let result = "ok";      // ES-6
+const P1 = 50;      // ES-6
+
+// score = 20;
+// result = 55;
+// P1 = 0.35;   // Const never changes  the value of a variable once it is assigned
+
+console.log(score);
+console.log(result);
+console.log(P1);
+
+//JAvaScript uses BEDMAS rule 
+
+let finalscore = score + result;
+console.log(finalscore);
+console.log(typeof finalscore);
+
